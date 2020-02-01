aef5918cd5b331dfa035fdbbd456391785f13cca
Amanda Trang <at669@cornell.edu>
2020-01-31 22:23:11 -0500

    2 sided platforming

---
+++ GameJam/Assets/Scripts/VetPlatformController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VetPlatformController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public bool isGrounded = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+        BetterJump();
+    }
+
+
+    void Move() {
+        float x = 0.0f;
+        if (Input.GetKey(KeyCode.LeftArrow)){ x = -1.0f; }
+        else if (Input.GetKey(KeyCode.RightArrow)){ x = 1.0f;}
+        float moveBy = x * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+
+    void Jump() {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && (isGrounded)) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    void BetterJump() {
+        if (rb.velocity.y < 0) {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        } else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space)) {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }   
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.tag == "Ground") {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision){
+        if (collision.gameObject.tag == "Ground") {
+            isGrounded = false;
+        }
+    }
+}
